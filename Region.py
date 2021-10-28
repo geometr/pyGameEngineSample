3b67855c85483e0c60032902d06ea163f83feff7
Kurochkin Konstantin <hq@aveslab.ru>
2021-10-28 16:44:09 +0400

    Реализован вывод спрайтов с поправкой на размер спрайта

--- Region.py
+++ Region.py
@@ -6,8 +6,10 @@ import RegionSprites
 
 class Region:
     region_size = 20
+    x_offset = int(20/2)
+    y_offset = int(10/2)
     def __init__(self, set_screen_offset_x = 300, set_screen_offset_y = 50, set_max_biom = 1, set_min_biom = 0):
-        self.level_map = array.array('b')
+        self.level_map = array.array('H')
         self.screen_offset_x = set_screen_offset_x
         self.screen_offset_y = set_screen_offset_y
         self.max_biom = set_max_biom
@@ -20,7 +22,19 @@ class Region:
                 self.level_map.append(random.randrange(0 + self.min_biom,self.max_biom))
                 j =j + 1
             i = i + 1
+        if self.min_biom == 1 and self.max_biom == 2:
+            t = 0
+            while t < 10:
+                x = random.randrange(0,self.region_size)
+                y = random.randrange(0,self.region_size)
+                self.level_map[int(x + y * self.region_size)] = self.sprites.TREE
+                t = t + 1
+            self.level_map[int(self.region_size/2 + self.region_size * ( self.region_size /2 -1))]=self.sprites.SHAVAR_ALDAN
 
+        if self.min_biom == 1 and self.max_biom == 3:
+            self.level_map[int(self.region_size/2 + self.region_size * ( self.region_size /2 -1))]=self.sprites.SHAVAR
+        if self.min_biom == 0 and self.max_biom == 2:
+            self.level_map[int(self.region_size/2 + self.region_size * ( self.region_size /2 -1))]=self.sprites.SHAVAR_ALDAN
     def notifyDayChanged(self, *args):
         pass
 
@@ -39,14 +53,19 @@ class Region:
                 self.sprites.times[type] = sprite
 
     def notifyScreenRender(self, screen):
-        x_offset = self.sprites.width / 2
-        y_offset = self.sprites.height / 2
         y = 0
         while y < self.region_size:
             x = 0
             while x < self.region_size:
+                xoff = 0
+                yoff = 0
                 tile = self.level_map[x + y * self.region_size]
                 sprite = self.sprites.times[int(tile)]
-                screen.draw(sprite, (x - y) * x_offset + self.screen_offset_x, (x + y) * y_offset + self.screen_offset_y)
+                sprite_xoff, sprite_yoff = self.sprites.sizes[int(tile)]
+                if (int(sprite_xoff/2) != self.x_offset):
+                    xoff = -sprite_xoff + self.x_offset*2
+                if (int(sprite_yoff/2) != self.y_offset):
+                    yoff = -sprite_yoff + self.y_offset*2
+                screen.draw(sprite, (x - y) * self.x_offset + xoff + self.screen_offset_x, yoff + (x + y) * self.y_offset + self.screen_offset_y)
                 x = x + 1
             y = y + 1
--- RegionSprites.py
+++ RegionSprites.py
@@ -6,6 +6,10 @@ class RegionSprites:
     DESERT = 2
     LAVA = 3
 
+    SHAVAR = 1000
+    SHAVAR_ALDAN = 1001
+    SHAVAR_GOO = 1002
+    TREE = 1003
     def __init__(self):
         self.sprites = dict()
         self.night = dict()
@@ -13,35 +17,56 @@ class RegionSprites:
         self.morning = dict()
         self.evening = dict()
         self.times = dict()
+        self.sizes = dict()
+        self.night_filter = dict()
+        self.day_filter = dict()
+        self.morning_filter = dict()
+        self.evening_filter = dict()
 
         self.sprites[self.TERRAIN] = pygame.image.load('../assets/tile_grass.png').convert_alpha()
+        self.sizes[self.TERRAIN] = (20,10)
         self.sprites[self.WATER] = pygame.image.load('../assets/tile_water.png').convert_alpha()
+        self.sizes[self.WATER] = (20,10)
         self.sprites[self.DESERT] = pygame.image.load('../assets/tile_sand.png').convert_alpha()
+        self.sizes[self.DESERT] = (20,10)
         self.sprites[self.LAVA] = pygame.image.load('../assets/tile_lava.png').convert_alpha()
-        self.prepareSprites()
+        self.sizes[self.LAVA] = (20,10)
 
+        self.sprites[self.SHAVAR] = pygame.image.load('../assets/tile_shavar.png').convert_alpha()
+        self.sizes[self.SHAVAR] = (20,10)
+        self.sprites[self.SHAVAR_ALDAN] = pygame.image.load('../assets/tile_shavar_aldan.png').convert_alpha()
+        self.sizes[self.SHAVAR_ALDAN] = (20,10)
+        self.sprites[self.SHAVAR_GOO] = pygame.image.load('../assets/tile_shavar_goo.png').convert_alpha()
+        self.sizes[self.SHAVAR_GOO] = (20,10)
+
+        self.sprites[self.TREE] = pygame.image.load('../assets/tile_tree.png').convert_alpha()
+        self.sizes[self.TREE] = (20,30)
+
+        self.prepareSprites()
         for type,sprite in self.night.items():
             self.times[type] = sprite
 
 
     def prepareSprites(self):
-        size = self.width, self.height = (20, 10)
-        self.sprite_night_filter = pygame.Surface(size).convert_alpha()
-        self.sprite_night_filter.fill((148,148,128,0))
-        self.sprite_day_filter = pygame.Surface(size).convert_alpha()
-        self.sprite_day_filter.fill((16,0,16,0))
-        self.sprite_morning_filter = pygame.Surface(size).convert_alpha()
-        self.sprite_morning_filter.fill((64,32,32,0))
-        self.sprite_evening_filter = pygame.Surface(size).convert_alpha()
-        self.sprite_evening_filter.fill((32,64,64,0))
+        for type, (width,height) in self.sizes.items():
+            self.night_filter[type] = pygame.Surface((width,height)).convert_alpha()
+            self.night_filter[type].fill((148,148,128,0))
+            self.day_filter[type] = pygame.Surface((width,height)).convert_alpha()
+            self.day_filter[type].fill((16,0,16,0))
+            self.morning_filter[type] = pygame.Surface((width,height)).convert_alpha()
+            self.morning_filter[type].fill((64,32,32,0))
+            self.evening_filter[type] = pygame.Surface((width,height)).convert_alpha()
+            self.evening_filter[type].fill((32,64,64,0))
 
         for type,sprite in self.sprites.items():
             self.morning[type] = sprite.copy()
             self.day[type] = sprite.copy()
             self.night[type] = sprite.copy()
             self.evening[type] = sprite.copy()
+            if type != self.LAVA or type != self.TREE:
+                self.morning[type].blit(self.morning_filter[type], (0,0), None, pygame.BLEND_RGBA_SUB)
+                self.evening[type].blit(self.evening_filter[type], (0,0), None, pygame.BLEND_RGBA_SUB)
+                self.night[type].blit(self.night_filter[type], (0,0), None, pygame.BLEND_RGBA_SUB)
+                if type !=self.TREE:
+                    self.day[type].blit(self.day_filter[type], (0,0), None, pygame.BLEND_RGBA_SUB)
 
-            self.morning[type].blit(self.sprite_morning_filter, (0,0), None, pygame.BLEND_RGBA_SUB)
-            self.day[type].blit(self.sprite_day_filter, (0,0), None, pygame.BLEND_RGBA_SUB)
-            self.evening[type].blit(self.sprite_evening_filter, (0,0), None, pygame.BLEND_RGBA_SUB)
-            self.night[type].blit(self.sprite_night_filter, (0,0), None, pygame.BLEND_RGBA_SUB)
